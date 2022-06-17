5a0d43319730783330997a038e590d406cab7add
logandouglass <logandouglass@gmail.com>
2022-06-17 15:37:13 -0400

    need to write a quick download page, and then back to music <3>

--- userinput/views.py
+++ userinput/views.py
@@ -76,11 +76,13 @@ def index(request):
     # form = ProgressionForm() # Won't need this if it works
     # context = {"form":form}
     context = {}
-    return render(request, "userinput/index-remaster.html", context)
+    return render(request, "userinput/index-remaster-2.html", context)
 
 def track(request, id):
     ## Get progression data and create model
 
+    broken_values = ["", None, 0]
+    
     composition = Composition.objects.get(id=id)
 
     comp_data_dict_list = list(Composition.objects.filter(id=id).values())
@@ -94,6 +96,15 @@ def track(request, id):
         chord1_bars_u = ""
     chord1_dg = [chord1_tonic, chord1_quality, chord1_bars]
     chord1_vis = False
+    
+    chord1_works = True
+    chord1_vals = [chord1_tonic, chord1_quality, chord1_bars]
+    for broken_value in broken_values:
+        if broken_value in chord1_vals:
+            chord1_works = False
+
+
+
 
     chord2_tonic = comp_data_dict["chord2_tonic"]
     chord2_quality = comp_data_dict["chord2_quality"]
@@ -104,6 +115,11 @@ def track(request, id):
         chord2_bars_u = ""
     chord2_dg = [chord2_tonic, chord2_quality, chord2_bars]
     chord2_vis = False
+    chord2_works = True
+    chord2_vals = [chord2_tonic, chord2_quality, chord2_bars]
+    for broken_value in broken_values:
+        if broken_value in chord2_vals:
+            chord2_works = False
 
     chord3_tonic = comp_data_dict["chord3_tonic"]
     chord3_quality = comp_data_dict["chord3_quality"]
@@ -114,6 +130,11 @@ def track(request, id):
         chord3_bars_u = ""
     chord3_dg = [chord3_tonic, chord3_quality, chord3_bars]
     chord3_vis = False
+    chord3_works = True
+    chord3_vals = [chord3_tonic, chord3_quality, chord3_bars]
+    for broken_value in broken_values:
+        if broken_value in chord3_vals:
+            chord3_works = False
 
     chord4_tonic = comp_data_dict["chord4_tonic"]
     chord4_quality = comp_data_dict["chord4_quality"]
@@ -124,6 +145,11 @@ def track(request, id):
         chord4_bars_u = ""
     chord4_dg = [chord4_tonic, chord4_quality, chord4_bars]
     chord4_vis = False
+    chord4_works = True
+    chord4_vals = [chord4_tonic, chord4_quality, chord4_bars]
+    for broken_value in broken_values:
+        if broken_value in chord4_vals:
+            chord4_works = False
 
     chord5_tonic = comp_data_dict["chord5_tonic"]
     chord5_quality = comp_data_dict["chord5_quality"]
@@ -134,6 +160,11 @@ def track(request, id):
         chord5_bars_u = ""
     chord5_dg = [chord5_tonic, chord5_quality, chord5_bars]
     chord5_vis = False
+    chord5_works = True
+    chord5_vals = [chord5_tonic, chord5_quality, chord5_bars]
+    for broken_value in broken_values:
+        if broken_value in chord5_vals:
+            chord5_works = False
 
     if "" not in chord1_dg:
         chord1_vis = True
@@ -148,6 +179,9 @@ def track(request, id):
 
     all_tracks = Track.objects.all()
     track_objs = all_tracks.filter(comp=composition)
+    no_tracks = False
+    if len(track_objs) == 0:
+        no_tracks = True
     track_names = []
     for track in track_objs:
         trackn = track.trackname
@@ -189,6 +223,12 @@ def track(request, id):
         "chord4_vis": chord4_vis,
         "chord5_vis": chord5_vis,
         "track_names": track_names,
+        "no_tracks": no_tracks,
+        "chord1_works": chord1_works,
+        "chord2_works": chord2_works,
+        "chord3_works": chord3_works,
+        "chord4_works": chord4_works,
+        "chord5_works": chord5_works,
     
     
     
@@ -198,15 +238,25 @@ def track(request, id):
 def trackadd(request, id):
     trackname = request.POST.get('trackname')
     chord1_style = request.POST.get('chord1_style')
-    chord1_denom = int(request.POST.get('chord1_denom'))
+    chord1_denom = 0
+    chord2_denom = 0
+    chord3_denom = 0
+    chord4_denom = 0
+    chord5_denom = 0
+    if request.POST.get('chord1_denom') != None:
+        chord1_denom = int(request.POST.get('chord1_denom'))
     chord2_style = request.POST.get('chord2_style')
-    chord2_denom = int(request.POST.get('chord2_denom'))
+    if request.POST.get('chord2_denom') != None:
+        chord2_denom = int(request.POST.get('chord2_denom'))
     chord3_style = request.POST.get('chord3_style')
-    chord3_denom = int(request.POST.get('chord3_denom'))
+    if request.POST.get('chord3_denom') != None:
+        chord3_denom = int(request.POST.get('chord3_denom'))
     chord4_style = request.POST.get('chord4_style')
-    chord4_denom = int(request.POST.get('chord4_denom'))
+    if request.POST.get('chord4_denom') != None:
+        chord4_denom = int(request.POST.get('chord4_denom'))
     chord5_style = request.POST.get('chord5_style')
-    chord5_denom = int(request.POST.get('chord5_denom'))
+    if request.POST.get('chord5_denom') != None:
+        chord5_denom = int(request.POST.get('chord5_denom'))
 
     Track.objects.create(
             comp = Composition.objects.get(id=id),    
