ee556cc0b4187304cc747a4d31c66b665e07a99e
logandouglass <logandouglass@gmail.com>
2022-06-21 14:37:04 -0400

    it never endsss

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from msilib.schema import Media
 import os
 from pathlib import Path
 
