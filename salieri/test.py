38b2800ef00b191a9a29e8e1de5e5c97d33cc6ab
logandouglass <logandouglass@gmail.com>
2022-09-13 12:19:35 -0400

    back to the ole salieri

--- salieri/test.py
+++ salieri/test.py
@@ -2,10 +2,14 @@ from Sfunctions import *
 
 bar = Bar()
 note = Note("C", 4)
-for _ in range(23):
-    bar.place_notes(note, 24)
+# for _ in range(23):
+#     bar.place_notes(note, 24)
+
+bar.place_notes(note, 8)
 derp = bar.current_beat
 print(derp)
-print(1-derp)
-print(1/24)
+new_denom = 1/derp
+print(new_denom)
+# print(1-derp)
+# print(1/24)
 # print()
